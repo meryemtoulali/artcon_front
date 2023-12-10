aebb6d68241331f678f61e6cb9416b31bd20d7d2
Salmaelf02 <salmaelfekak02@gmail.com>
2023-12-11 00:23:40 +0100

    Gender & Location Spinners work!

--- app/src/main/java/com/example/artcon_test/SignUpSecondFragment.java
+++ app/src/main/java/com/example/artcon_test/SignUpSecondFragment.java
@@ -2,25 +2,42 @@ package com.example.artcon_test;
 
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.artcon_test.model.Location;
+import com.example.artcon_test.network.LocationService;
+import com.example.artcon_test.viewmodel.LocationViewModel;
 import com.example.artcon_test.viewmodel.SignupViewModel;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class SignUpSecondFragment extends Fragment {
-    private EditText firstNameEditText, lastNameEditText, genderEditText,phoneEditText, birthdayEditText, locationEditText;
+    private EditText firstNameEditText, lastNameEditText,phoneEditText, birthdayEditText;
     SignupViewModel signupViewModel;
+    private Spinner genderSpinner;
+    private Spinner locationSpinner;
+    private ArrayAdapter<CharSequence> genderAdapter;
+    private ArrayAdapter<String> locationAdapter;
+    private List<Location> locations = new ArrayList<>();
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_signup_second, container, false);
@@ -28,10 +45,19 @@ public class SignUpSecondFragment extends Fragment {
 
         firstNameEditText = view.findViewById(R.id.editTextFirstNameSignUp);
         lastNameEditText = view.findViewById(R.id.editTextLastNameSignUp);
-        genderEditText = view.findViewById(R.id.editTextGenderSignUp);
+        //genderEditText = view.findViewById(R.id.editTextGenderSignUp);
         phoneEditText = view.findViewById(R.id.editTextPhoneSignUp);
         birthdayEditText = view.findViewById(R.id.editTextBirthdaySignUp);
-        locationEditText = view.findViewById(R.id.editTextLocationSignUp);
+        //locationEditText = view.findViewById(R.id.spinnerLocationSignUp);
+
+        genderSpinner = view.findViewById(R.id.spinnerGenderSignUp);
+        genderAdapter = ArrayAdapter.createFromResource(requireContext(), R.array.gender_array, android.R.layout.simple_spinner_item);
+        genderAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        genderSpinner.setAdapter(genderAdapter);
+
+        locationSpinner = view.findViewById(R.id.spinnerLocationSignUp);
+        Log.d("location2","i am here 1");
+        populateLocationDropdown();
 
         Button signUpButton = view.findViewById(R.id.buttonSignUp);
         signUpButton.setOnClickListener(new View.OnClickListener() {
@@ -56,12 +82,55 @@ public class SignUpSecondFragment extends Fragment {
 
         return view;
     }
+
+    private void populateLocationDropdown() {
+        LocationService locationService = LocationViewModel.getLocationService();
+        Call<List<Location>> call = locationService.getLocations();
+        Log.d("location2","Here i am by the call");
+        call.enqueue(new Callback<List<Location>>() {
+            @Override
+            public void onResponse(Call<List<Location>> call, Response<List<Location>> response) {
+                if (response.isSuccessful()) {
+                    Log.d("location2","success");
+                    locations = response.body();
+                    setupLocationDropdown();
+                } else {
+                    Log.d("location","location response has failed!");
+                }
+            }
+
+            private void setupLocationDropdown() {
+                List<String> locationNames = new ArrayList<>();
+                Log.d("location2","success setup");
+                locationNames.add(getString(R.string.select_location_hint)); // Add a hint or default value
+
+                for (Location location : locations) {
+                    locationNames.add(location.getName());
+                }
+
+                locationAdapter = new ArrayAdapter<>(
+                        requireContext(),
+                        android.R.layout.simple_spinner_item,
+                        locationNames
+                );
+                locationAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+                locationSpinner.setAdapter(locationAdapter);
+            }
+
+            @Override
+            public void onFailure(Call<List<Location>> call, Throwable t) {
+                Log.e("location2", "Huge Error: " + t.getMessage());
+            }
+        });
+    }
+
     private boolean isValidInput() {
         return !TextUtils.isEmpty(firstNameEditText.getText())
                 && !TextUtils.isEmpty(lastNameEditText.getText())
                 && !TextUtils.isEmpty(birthdayEditText.getText())
-                && !TextUtils.isEmpty(genderEditText.getText())
-                && !TextUtils.isEmpty(locationEditText.getText())
+                && !TextUtils.isEmpty(genderSpinner.getSelectedItem().toString())
+                && !TextUtils.isEmpty(locationSpinner.getSelectedItem().toString())
                 && !TextUtils.isEmpty(phoneEditText.getText());
     }
     public String getFirstName() {
@@ -81,7 +150,7 @@ public class SignUpSecondFragment extends Fragment {
     public Date getBirthday() {
         if (birthdayEditText != null) {
             String dateString = birthdayEditText.getText().toString();
-            SimpleDateFormat dateFormat = new SimpleDateFormat("mm/dd/yyyy"); // Adjust the pattern based on your date format
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd"); // Adjust the pattern based on your date format
 
             try {
                 return dateFormat.parse(dateString);
@@ -94,19 +163,29 @@ public class SignUpSecondFragment extends Fragment {
 
     // Method to get the location from the EditText field
     public String getLocation() {
-        if (locationEditText != null) {
-            return locationEditText.getText().toString();
+        if (locationSpinner != null) {
+            String selectedLocation = locationSpinner.getSelectedItem().toString();
+            if (!selectedLocation.equals(getString(R.string.select_gender_hint))) {
+                return selectedLocation;
+            } else {
+                return null;
+            }
         } else {
-            return null; // Handle the case when the locationEditText is not initialized
+            return null; // Handle the case when the genderSpinner is not initialized
         }
     }
 
     // Method to get the gender from the EditText field
     public String getGender() {
-        if (genderEditText != null) {
-            return genderEditText.getText().toString();
+        if (genderSpinner != null) {
+            String selectedGender = genderSpinner.getSelectedItem().toString();
+            if (!selectedGender.equals(getString(R.string.select_gender_hint))) {
+                return selectedGender;
+            } else {
+                return null;
+            }
         } else {
-            return null; // Handle the case when the genderEditText is not initialized
+            return null; // Handle the case when the genderSpinner is not initialized
         }
     }
 
---
+++ app/src/main/java/com/example/artcon_test/network/LocationService.java
@@ -0,0 +1,14 @@
+package com.example.artcon_test.network;
+
+import com.example.artcon_test.model.Location;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface LocationService {
+    @GET("location/all")
+    Call<List<Location>> getLocations();
+
+}
