619d0ff3cfcc512ab45483d8317440144e2a7cb7
meryemtoulali <toulalimeryem@gmail.com>
2024-01-14 00:13:17 +0100

    add logout to home

--- app/src/main/java/com/example/artcon_test/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/artcon_test/ui/home/HomeFragment.java
@@ -2,12 +2,17 @@ package com.example.artcon_test.ui.home;
 
 import static android.content.Context.MODE_PRIVATE;
 
+import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -19,8 +24,10 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.artcon_test.R;
 import com.example.artcon_test.databinding.FragmentHomeBinding;
+import com.example.artcon_test.ui.login.LoginActivity;
 import com.example.artcon_test.ui.profile.PostAdapter;
 import com.example.artcon_test.ui.search.PeopleAdapter;
+import com.example.artcon_test.viewmodel.LogoutViewModel;
 
 public class HomeFragment extends Fragment implements PostAdapter.OnUserAreaClickListener {
 
@@ -30,12 +37,16 @@ public class HomeFragment extends Fragment implements PostAdapter.OnUserAreaClic
     private RecyclerView recyclerView;
     HomeViewModel homeViewModel;
     private PostAdapter postAdapter;
+    private ImageView kebabMenu;
+    private LogoutViewModel logoutViewModel;
+
 
     //On create method
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
+        logoutViewModel = new ViewModelProvider(this).get(LogoutViewModel.class);
         SharedPreferences preferences = requireActivity().getSharedPreferences("AuthPrefs", MODE_PRIVATE);
         userId=preferences.getString("userId",null);
     }
@@ -49,6 +60,7 @@ public class HomeFragment extends Fragment implements PostAdapter.OnUserAreaClic
 
         recyclerView = binding.feed;
 
+        kebabMenu = binding.kebabMenu;
         LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
         recyclerView.setLayoutManager(layoutManager);
         postAdapter = new PostAdapter(getContext());
@@ -62,6 +74,24 @@ public class HomeFragment extends Fragment implements PostAdapter.OnUserAreaClic
             postAdapter.setPostList(postList);
             postAdapter.notifyDataSetChanged();
         });
+
+        kebabMenu.setOnClickListener(v -> {
+            PopupMenu popup = new PopupMenu(requireContext(), v);
+            popup.getMenuInflater().inflate(R.menu.kebab_menu, popup.getMenu());
+            popup.setOnMenuItemClickListener(item -> {
+                if (item.getItemId() == R.id.menu_logout) {
+                    logoutViewModel.logout();
+                    Toast.makeText(requireContext(), "Logging out...", Toast.LENGTH_SHORT).show();
+                    navigateToLoginPage();
+                    clearUserSession();
+                    return true;
+                }
+                Toast.makeText(requireContext(), "Network Error", Toast.LENGTH_SHORT).show();
+                return false;
+            });
+            popup.show();
+        });
+
         return root;
     }
 
@@ -70,6 +100,19 @@ public class HomeFragment extends Fragment implements PostAdapter.OnUserAreaClic
         Bundle args = new Bundle();
         args.putString("selectedUserId", userId);
         Navigation.findNavController(requireView()).navigate(R.id.action_home_to_profile, args);
+    }
+
+
+    private void clearUserSession() {
+        SharedPreferences preferences = requireContext().getSharedPreferences("AuthPrefs", Context.MODE_PRIVATE);
+        preferences.edit().clear().apply();
+    }
+
+    private void navigateToLoginPage() {
+        Intent intent = new Intent(requireContext(), LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        requireActivity().finish();
     }
 
     @Override
